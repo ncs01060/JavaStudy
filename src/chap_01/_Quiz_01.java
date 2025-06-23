92b6bc2004098e8e5e6eadb986ed5228734d1cc8
ncs01060 <codingmin@icloud.com>
2025-06-23 17:26:58 +0900

    Quiz1

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        final String BUS_NAME = "상암08";
+        int remainingTime = 3;
+        float remainingDistance = 1.5F;
+
+        System.out.println(BUS_NAME + "번 버스");
+        System.out.println("약" + remainingTime + "분 후 도착");
+        System.out.println("남은 거리" + remainingDistance + "km");
+    }
+}
